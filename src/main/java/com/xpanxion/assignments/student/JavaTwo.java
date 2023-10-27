026a66700efe94c178b50f7de1a57026ceff78ef
elanadanseglio <148129709+elanadanseglio@users.noreply.github.com>
2023-10-27 14:13:52 -0400

    Update JavaTwo.java

--- src/main/java/com/xpanxion/assignments/student/JavaTwo.java
+++ src/main/java/com/xpanxion/assignments/student/JavaTwo.java
@@ -68,12 +68,58 @@ public class JavaTwo {
     }
     
     public void ex4() {
+        var invoice = new Invoice(1);
+        invoice.addProduct(new Product(111,"Mustard", 2.00));
+        invoice.addProduct(new Product(222,"Ketchup", 3.00));
+        invoice.addProduct(new Product(333,"Franks Hot Sauce", 4.00));
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        System.out.println("Total cost: " + formatter.format(invoice.getTotalCost()));
     }
     
     public void ex5() {
+        var repository = new Repository();
+        var p = repository.getPerson();
+        System.out.println(p);
     }
     
     public void ex6() {
+        Scanner console = new Scanner(System.in);
+        String firstNum = " ";
+        String secondNum = " ";
+        int result = 0;
+        String op = "";
+
+        var calculator =  new Calculator();
+        while ((firstNum.charAt(0) != 'd') || (secondNum.charAt(0) != 'd') || (!op.equals("done"))) {
+            System.out.print("Enter first number: ");
+            firstNum = console.nextLine();
+            if (firstNum.charAt(0) == 'd'){
+                break;
+            }
+            System.out.print("Enter second number: ");
+            secondNum = console.nextLine();
+            if (secondNum.charAt(0) == 'd'){
+                break;
+            }
+            System.out.print("Enter operation (add, sub, mul, div): ");
+            op = console.nextLine();
+
+            if (op.equals("d")){
+                break;
+            }
+
+            if (op.equals("add")){
+                result = calculator.add(Integer.parseInt(firstNum), Integer.parseInt(secondNum));
+            } else if (op.equals("sub")){
+                result = calculator.subtract(Integer.parseInt(firstNum), Integer.parseInt(secondNum));
+            } else if (op.equals("mul")){
+                result = calculator.multiply(Integer.parseInt(firstNum), Integer.parseInt(secondNum));
+            } else if (op.equals("div")){
+                result = calculator.divide(Integer.parseInt(firstNum), Integer.parseInt(secondNum));
+            }
+            System.out.println("Result: " + result);
+        }
+        System.out.println(calculator.getHistory()); 
     }
     
     public void ex7() {
