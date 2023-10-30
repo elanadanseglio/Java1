00382fbdf0a1a738eb9d5ed1ad9e44be6a113c5f
elanadanseglio <148129709+elanadanseglio@users.noreply.github.com>
2023-10-29 21:50:26 -0400

    Update JavaTwo.java

--- src/main/java/com/xpanxion/assignments/student/JavaTwo.java
+++ src/main/java/com/xpanxion/assignments/student/JavaTwo.java
@@ -1,10 +1,15 @@
 package com.xpanxion.assignments.student;
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 public class JavaTwo {
 
     //
@@ -123,15 +128,64 @@ public class JavaTwo {
     }
     
     public void ex7() {
+        var personList = Arrays.asList(
+        new Person(1, "Peter", "Jones"),
+        new Person(2, "John", "Smith"),
+        new Person(3, "Sue", "Anderson"));
+
+        var newPersonList = personList.stream().map(person -> new Person(person.getID(), person.getFirstName(), "xxx")).toList();
+        for (Person p : newPersonList){
+            System.out.println(p);
+        }
     }
     
     public void ex8() {
+        var personList = Arrays.asList(
+        new Person(1, "Charlie", "Jones"),
+        new Person(2, "Zoey", "Smith"),
+        new Person(3, "Adam", "Anderson")
+        );
+
+        personList = personList.stream().sorted(Comparator.comparing(Person::getFirstName)).toList();
+
+        for (Person p : personList) {
+            System.out.println(p);
+        }
     }
     
     public void ex9() {
-    }
+        var personList = Arrays.asList(
+        new Person(1, "Charlie", "Jones"),
+        new Person(2, "Zoey", "Smith"),
+        new Person(3, "Adam", "Anderson")
+        );
+
+        var filteredList = personList.stream().filter(person -> person.getLastName() == "Smith").toList();
+
+        for (Person p : filteredList) {
+            System.out.println(p);
+        }
+    }   
     
     public void ex10() {
+        Queue<Cat> catQueue = new LinkedList<>();
+
+        catQueue.add(new Cat("Alice"));
+        catQueue.add(new Cat("Bob"));
+        catQueue.add(new Cat("Charlie"));
+        catQueue.add(new Cat("Dan"));
+
+        while (!catQueue.isEmpty()){
+            Cat cat = catQueue.peek();
+            System.out.println(catQueue);
+            catQueue.remove();  
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+        }
     }
 
 }
